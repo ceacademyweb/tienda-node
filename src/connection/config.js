ce53c9c3c6952f3c8fb7b794ec355929a799e75e
ceacademyweb <ceacademy.web@gmail.com>
2023-06-15 13:31:05 -0600

    first commit

--- src/connection/config.js
+++ src/connection/config.js
@@ -1,21 +1,21 @@
+// module.exports = {
+//   mysqldb: {
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'crud_api',
+//   },
+//   mongodb: {
+//     host: env.MONGOHOST || 'localhost',
+//     user: env.MONGOUSER || '',
+//     password: env.MONGOPASSWORD || '',
+//     database: 'ceacademy_db',
+//     port: env.MONGOPORT || 27017,
+//   },
+// };
 module.exports = {
-  mysqldb: {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'crud_api',
-  },
-  mongodb: {
-    host: env.MONGOHOST || 'localhost',
-    user: env.MONGOUSER || '',
-    password: env.MONGOPASSWORD || '',
-    database: 'ceacademy_db',
-    port: env.MONGOPORT || 27017,
-  },
+  MONGOHOST: 'containers-us-west-138.railway.app',
+  MONGOUSER: 'mongo',
+  MONGOPASSWORD: 'A3xJRdUYsO7E1CkF64MC',
+  MONGOPORT: '7615',
 };
-// {
-//   "MONGOHOST": "containers-us-west-138.railway.app",
-//   "MONGOUSER": "mongo",
-//   "MONGOPASSWORD": "A3xJRdUYsO7E1CkF64MC"
-//   "MONGOPORT" : "7615"
-// }
