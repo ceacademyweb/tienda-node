8dcbe087d84df44f07ce322ba8f2ff053baf03e3
ceacademyweb <ceacademy.web@gmail.com>
2023-06-15 12:46:37 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ app.set('port', process.env.PORT || 5000);
 
 // console.log('Starting');
 
-// app.get('/', function (req, res) {
-//   res.send('funcionando');
-// });
+app.get('/', function (req, res) {
+  res.send('funcionando');
+});
 
 app.listen(app.get('port'), () => {
   console.log(`app run on port ${app.get('port')}`);
